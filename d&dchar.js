bc3d666ae7b65d5eb5a374d90b7337b9855fe31a
VitorGoatman <vitor.goatman@gmail.com>
2020-11-11 17:30:55 -0300

    Update

--- d&dchar.js
+++ d&dchar.js
@@ -9,8 +9,35 @@ class Atributos {
     }
     get statblock() {
         var txt = "STR: " + this.strenght +"<br>DEX: " + this.dexterity + "<br>CON: " + this.constitution + "<br>INT: " + this.intelligence + "<br>WIS: " + this.wisdom + "<br>CHA: " + this.charisma;
-        return txt;
+        return "Atributos:<br>"+txt+"<br>";
     }
 }
-GalikStats = new Atributos(13, 16, 14, 10, 16, 11);
-document.getElementById('resultado').innerHTML = GalikStats.statblock;
+function getSTR() {
+    var score = document.getElementById('STR').value;
+    return score;
+}
+function getDEX() {
+    var score = document.getElementById('DEX').value;
+    return score;
+}
+function getCON() {
+    var score = document.getElementById('CON').value;
+    return score;
+}
+function getINT() {
+    var score = document.getElementById('INT').value;
+    return score;
+}
+function getWIS() {
+    var score = document.getElementById('WIS').value;
+    return score;
+}
+function getCHA() {
+    var score = document.getElementById('CHA').value;
+    return score;
+}
+
+CharStats = new Atributos(getSTR(), getDEX(), getCON(), getINT(), getWIS(), getCHA());
+function printStuff() {
+    document.getElementById('resultado').innerHTML = CharStats.statblock;
+}
